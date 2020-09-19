d61e269a5c107cdd6594012202ee104d55120534
chakri01 <57781113+chakri01@users.noreply.github.com>
2020-09-19 10:40:31 -0400

    Add files via upload

---
+++ anishhh/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("can u find 5 secrets of anish");
+  });
+var x2=img2.addEventListener("click",()=>alert('helping nature lo mother teresa'));
+var x3=img3.addEventListener("click",()=>alert('vocabulary master'));
+var x4=img4.addEventListener("click",()=>alert('man on time'));
+var x5=img5.addEventListener("click",()=>alert('always follows rules'));
+var x1=img1.addEventListener("click",()=>alert('never gets angry'));
+
